498f7f4d74b1584f4ad57c1dcf9b3a5ea20a4d08
Therese C Nordnes <therese@getacademy.no>
2026-04-10 20:10:17 +0200

    redirect

--- js/app.js
+++ js/app.js
@@ -3,6 +3,13 @@ import Header from "../components/header.js";
 import Footer from "../components/footer.js";
 import { initMobileNav } from "../components/header.js";
 
+const redirect = sessionStorage.getItem("redirect");
+
+if (redirect) {
+  sessionStorage.removeItem("redirect");
+  history.replaceState(null, null, redirect);
+}
+
 document.getElementById("header").innerHTML = Header();
 document.getElementById("footer").innerHTML = Footer();
 
